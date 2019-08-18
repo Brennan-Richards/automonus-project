cf15c6a26ec5dddf2cdbc9e249eddc10be07909c
Brennan-Richards <brennanr0210@gmail.com>
2019-08-18 15:25:42 -0400

    Removed django for heroku var.

--- automonus/settings.py
+++ automonus/settings.py
@@ -131,6 +131,3 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
