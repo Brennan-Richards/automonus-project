dabed86675da946ed22de630507c2615d968d1c0
Brennan-Richards <brennanr0210@gmail.com>
2019-08-18 17:31:11 -0400

    Removed extra static folder

--- automonus/settings.py
+++ automonus/settings.py
@@ -127,6 +127,6 @@ STATICFILES_DIRS = [
     #'/var/www/static/',
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
