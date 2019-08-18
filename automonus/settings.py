6a152add3f218b6eb14366ce828efc42297e094f
Brennan-Richards <brennanr0210@gmail.com>
2019-08-18 15:28:40 -0400

    Removed serene tundra as default host.

--- automonus/settings.py
+++ automonus/settings.py
@@ -26,9 +26,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['serene-tundra-76328.herokuapp.com/']
-
-
 # Application definition
 
 INSTALLED_APPS = [
