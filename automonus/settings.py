5a8926cd2baa49d9b10dbf894a6b77c58e9da258
Brennan-Richards <brennanr0210@gmail.com>
2019-08-18 20:13:05 -0400

    ..

--- automonus/settings.py
+++ automonus/settings.py
@@ -80,15 +80,7 @@ WSGI_APPLICATION = 'automonus.wsgi.application'
 DATABASES = {
 
     'default': dj_database_url.config()
-
-    # 'default': {
-    # 'ENGINE': 'django.db.backends.postgresql',
-    # 'NAME': 'template1',
-    # 'USER': 'brennanrichards',
-    # 'PASSWORD': 'F00tb@ll#',
-    # 'HOST': 'localhost',
-    # 'PORT': '5432',
-    # }
+    
 
 }
 
