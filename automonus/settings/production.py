dbc9d5047ef86fb99afde4b517e4f43943096cb4
Brennan-Richards <brennanr0210@gmail.com>
2019-08-20 12:39:04 -0400

    No more beta dark, settings back to debug False

--- automonus/settings/production.py
+++ automonus/settings/production.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'B$QKt.zEqom(]`Qe[cz>X!m@*O3Z:N'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['automonus-project.herokuapp.com']
 
