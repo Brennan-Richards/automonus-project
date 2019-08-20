d8b52d0aa7a2a749686a6237ea6d68dffac42dd9
Brennan-Richards <brennanr0210@gmail.com>
2019-08-20 00:45:36 -0400

    New beta production version (Fixed os_environ settings points to dev settings.)

--- automonus/wsgi.py
+++ automonus/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'automonus.settings.production_settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'automonus.settings.development')
 
 application = get_wsgi_application()
