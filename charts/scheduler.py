3dbe3642d152c827f79ae4b35c90e7e03f2c213c
Brennan-Richards <brennanr0210@gmail.com>
2019-11-03 22:01:27 -0500

    Bug fix in AP_Scheduler (/charts app) - change analysis -> charts

--- charts/scheduler.py
+++ charts/scheduler.py
@@ -20,13 +20,13 @@ def start():
     # - Add a scheduled job to the job store on application initialization
     # - The job will execute a model class method each hour of each day
     # - replace_existing in combination with the unique ID prevents duplicate copies of the job
-    scheduler.add_job("analysis:scheduled_jobs.accounts_snapshot.CreateAccountSnapshot", "cron", id="create_accounts_snapshots",
+    scheduler.add_job("charts:scheduled_jobs.accounts_snapshot.CreateAccountSnapshot", "cron", id="create_accounts_snapshots",
                       day_of_week='*', hour='*', replace_existing=True)
-    scheduler.add_job("analysis:scheduled_jobs.user_sec_holding_snapshot.CreateUserSecurityHoldingSnapshot", "cron", id="create_user_security_snapshot",
+    scheduler.add_job("charts:scheduled_jobs.user_sec_holding_snapshot.CreateUserSecurityHoldingSnapshot", "cron", id="create_user_security_snapshot",
                       day_of_week='*', hour='*', replace_existing=True)
-    scheduler.add_job("analysis:scheduled_jobs.student_loan_snapshot.CreateStudentLoanSnapshot", "cron", id="create_student_loan_snapshot",
+    scheduler.add_job("charts:scheduled_jobs.student_loan_snapshot.CreateStudentLoanSnapshot", "cron", id="create_student_loan_snapshot",
                       day_of_week='*', hour='*', replace_existing=True)
-    scheduler.add_job("analysis:scheduled_jobs.credit_card_snapshot.CreateCreditCardSnapshot", "cron", id="create_credit_card_snapshot",
+    scheduler.add_job("charts:scheduled_jobs.credit_card_snapshot.CreateCreditCardSnapshot", "cron", id="create_credit_card_snapshot",
                       day_of_week='*', hour='*', replace_existing=True)
     # Add the scheduled jobs to the Django admin interface
     register_events(scheduler)
